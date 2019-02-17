bd4b12d5985c319f56c8e665a8c85d358afc1110
Sakura Rapolu <rapolusakura@gmail.com>
2019-02-17 07:12:46 -0800

    First commit

---
+++ CoreMLSample/MapViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MapViewController.swift
+//  CoreMLSample
+//
+//  Created by Sakura Rapolu on 2/16/19.
+//  Copyright © 2019 杨萧玉. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MapViewController: UIViewController {
+    
+
+}
---
+++ CoreMLSample/ViewController.swift
@@ -0,0 +1,419 @@
+//
+//  ViewController.swift
+//  CoreMLSimple
+//
+//  Created by 杨萧玉 on 2017/6/9.
+//  Copyright © 2017年 杨萧玉. All rights reserved.
+//
+
+import UIKit
+import CoreMedia
+import Vision
+import Firebase
+import FirebaseDatabase
+import AVFoundation
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, AVCaptureFileOutputRecordingDelegate {
+    // Outlets to label and view
+    
+    @IBOutlet var camPreview: UIView!
+    @IBOutlet private weak var predictLabel: UILabel!
+    @IBOutlet private weak var previewView: UIView!
+    @IBOutlet private weak var visionSwitch: UISwitch!
+    @IBOutlet weak var imageView: UIImageView!
+    var db: Firestore!
+    
+    let cameraButton = UIView()
+    
+    let captureSession = AVCaptureSession()
+    
+    let movieOutput = AVCaptureMovieFileOutput()
+    
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    
+    var activeInput: AVCaptureDeviceInput!
+    
+    var outputURL: URL!
+
+    
+    // some properties used to control the app and store appropriate values
+    
+    let inceptionv3model = Inceptionv3()
+    private var videoCapture: VideoCapture!
+    private var requests = [VNRequest]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var ref: DatabaseReference! = Database.database().reference()
+        
+        let refHandle = ref.observe(DataEventType.value, with: { (snapshot) in
+            let postDict = snapshot.value as? [String : AnyObject] ?? [:]
+            print(postDict)
+        })
+        
+        if setupSession() {
+            setupPreview()
+            startSession()
+        }
+        
+        cameraButton.isUserInteractionEnabled = true
+        
+        let cameraButtonRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.startCapture))
+        
+        cameraButton.addGestureRecognizer(cameraButtonRecognizer)
+        
+        cameraButton.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        
+        cameraButton.backgroundColor = UIColor.red
+        
+        camPreview.addSubview(cameraButton)
+        camPreview.addSubview(imageView)
+        camPreview.addSubview(predictLabel)
+        
+        let singleTap = UITapGestureRecognizer(target: self,action:Selector("imageTapped"))
+        imageView.isUserInteractionEnabled = true
+        imageView.addGestureRecognizer(singleTap)
+        self.imageView.image = UIImage(named: "lol.png")
+        setupVision()
+        let spec = VideoSpec(fps: 100, size: CGSize(width: 299, height: 299))
+        videoCapture = VideoCapture(cameraType: .back,
+                                    preferredSpec: spec,
+                                    previewContainer: previewView.layer)
+        
+        videoCapture.imageBufferHandler = {[unowned self] (imageBuffer) in
+            if self.visionSwitch.isOn {
+                // Use Vision
+                self.handleImageBufferWithVision(imageBuffer: imageBuffer)
+            }
+            else {
+                // Use Core ML
+                self.handleImageBufferWithCoreML(imageBuffer: imageBuffer)
+            }
+        }
+    }
+    
+    func setupPreview() {
+        // Configure previewLayer
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = camPreview.bounds
+        previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        camPreview.layer.addSublayer(previewLayer)
+    }
+    
+    //MARK:- Setup Camera
+    
+    func setupSession() -> Bool {
+        
+        captureSession.sessionPreset = AVCaptureSession.Preset.high
+        
+        // Setup Camera
+        let camera = AVCaptureDevice.default(for: AVMediaType.video)!
+        
+        do {
+            
+            let input = try AVCaptureDeviceInput(device: camera)
+            
+            if captureSession.canAddInput(input) {
+                captureSession.addInput(input)
+                activeInput = input
+            }
+        } catch {
+            print("Error setting device video input: \(error)")
+            return false
+        }
+        
+        // Setup Microphone
+        let microphone = AVCaptureDevice.default(for: AVMediaType.audio)!
+        
+        do {
+            let micInput = try AVCaptureDeviceInput(device: microphone)
+            if captureSession.canAddInput(micInput) {
+                captureSession.addInput(micInput)
+            }
+        } catch {
+            print("Error setting device audio input: \(error)")
+            return false
+        }
+        
+        
+        // Movie output
+        if captureSession.canAddOutput(movieOutput) {
+            captureSession.addOutput(movieOutput)
+        }
+        
+        return true
+    }
+    
+    func setupCaptureMode(_ mode: Int) {
+        // Video Mode
+        
+    }
+    
+    //MARK:- Camera Session
+    func startSession() {
+        
+        if !captureSession.isRunning {
+            videoQueue().async {
+                self.captureSession.startRunning()
+            }
+        }
+    }
+    
+    func stopSession() {
+        if captureSession.isRunning {
+            videoQueue().async {
+                self.captureSession.stopRunning()
+            }
+        }
+    }
+    
+    func videoQueue() -> DispatchQueue {
+        return DispatchQueue.main
+    }
+    
+    func currentVideoOrientation() -> AVCaptureVideoOrientation {
+        var orientation: AVCaptureVideoOrientation
+        
+        switch UIDevice.current.orientation {
+        case .portrait:
+            orientation = AVCaptureVideoOrientation.portrait
+        case .landscapeRight:
+            orientation = AVCaptureVideoOrientation.landscapeLeft
+        case .portraitUpsideDown:
+            orientation = AVCaptureVideoOrientation.portraitUpsideDown
+        default:
+            orientation = AVCaptureVideoOrientation.landscapeRight
+        }
+        
+        return orientation
+    }
+    
+    @objc func startCapture() {
+        
+        startRecording()
+        
+    }
+    
+    //EDIT 1: I FORGOT THIS AT FIRST
+    
+    func tempURL() -> URL? {
+        let directory = NSTemporaryDirectory() as NSString
+        
+        if directory != "" {
+            let path = directory.appendingPathComponent(NSUUID().uuidString + ".mp4")
+            return URL(fileURLWithPath: path)
+        }
+        
+        return nil
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let vc = segue.destination as! VideoPlayback
+        
+        vc.videoURL = sender as? URL
+        
+    }
+    
+    func startRecording() {
+        
+        if movieOutput.isRecording == false {
+            
+            let connection = movieOutput.connection(with: AVMediaType.video)
+            
+            if (connection?.isVideoOrientationSupported)! {
+                connection?.videoOrientation = currentVideoOrientation()
+            }
+            
+            if (connection?.isVideoStabilizationSupported)! {
+                connection?.preferredVideoStabilizationMode = AVCaptureVideoStabilizationMode.auto
+            }
+            
+            let device = activeInput.device
+            
+            if (device.isSmoothAutoFocusSupported) {
+                
+                do {
+                    try device.lockForConfiguration()
+                    device.isSmoothAutoFocusEnabled = false
+                    device.unlockForConfiguration()
+                } catch {
+                    print("Error setting configuration: \(error)")
+                }
+                
+            }
+            
+            //EDIT2: And I forgot this
+            outputURL = tempURL()
+            movieOutput.startRecording(to: outputURL, recordingDelegate: self)
+            
+        }
+        else {
+            stopRecording()
+        }
+        
+    }
+    
+    func stopRecording() {
+        
+        if movieOutput.isRecording == true {
+            movieOutput.stopRecording()
+        }
+    }
+    
+    func capture(_ captureOutput: AVCaptureFileOutput!, didStartRecordingToOutputFileAt fileURL: URL!, fromConnections connections: [Any]!) {
+        
+    }
+    
+    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
+        
+        if (error != nil) {
+            
+            print("Error recording movie: \(error!.localizedDescription)")
+            
+        } else {
+            
+            let videoRecorded = outputURL! as URL
+            
+            performSegue(withIdentifier: "showVideo", sender: videoRecorded)
+            
+        }
+        
+    }
+    
+    func handleImageBufferWithCoreML(imageBuffer: CMSampleBuffer) {
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(imageBuffer) else {
+            return
+        }
+        do {
+            let prediction = try self.inceptionv3model.prediction(image: self.resize(pixelBuffer: pixelBuffer)!)
+            DispatchQueue.main.async {
+                if let prob = prediction.classLabelProbs[prediction.classLabel] {
+                    self.predictLabel.text = "\(prediction.classLabel) \(String(describing: prob))"
+                }
+            }
+        }
+        catch let error as NSError {
+            fatalError("Unexpected error ocurred: \(error.localizedDescription).")
+        }
+    }
+    
+    @objc func imageTapped() {
+        performSegue(withIdentifier: "clickedMap", sender: self)
+    }
+    
+    func handleImageBufferWithVision(imageBuffer: CMSampleBuffer) {
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(imageBuffer) else {
+            return
+        }
+        
+        var requestOptions:[VNImageOption : Any] = [:]
+        
+        if let cameraIntrinsicData = CMGetAttachment(imageBuffer, kCMSampleBufferAttachmentKey_CameraIntrinsicMatrix, nil) {
+            requestOptions = [.cameraIntrinsics:cameraIntrinsicData]
+        }
+        
+        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: CGImagePropertyOrientation(rawValue: UInt32(self.exifOrientationFromDeviceOrientation))!, options: requestOptions)
+        do {
+            try imageRequestHandler.perform(self.requests)
+        } catch {
+            print(error)
+        }
+    }
+    
+    func setupVision() {
+        let weapons: Set = ["gun", "revolver", "six-gun", "six-shooter", "pistol", "holster", "knife", "weapon", "rifle", "cleaver", "chopper", "paperknife", "paper knife", "assault rife", "assault gun", "hatchet", "scabbard", "guillotine", "letter opener, paper knife, paperknife", "revolver, six-gun, six-shooter", "assault rifle, assault gun"]
+        guard let visionModel = try? VNCoreMLModel(for: inceptionv3model.model) else {
+            fatalError("can't load Vision ML model")
+        }
+        let classificationRequest = VNCoreMLRequest(model: visionModel) { (request: VNRequest, error: Error?) in
+            guard let observations = request.results else {
+                print("no results:\(error!)")
+                return
+            }
+            
+            let classifications = observations[0...4]
+                .compactMap({ $0 as? VNClassificationObservation })
+                .filter({ $0.confidence >= 0.05 })
+                .filter({ weapons.contains($0.identifier)})
+                .map({ "\($0.identifier) \($0.confidence)" })
+            DispatchQueue.main.async {
+                print(classifications.joined(separator: "\n"))
+                self.predictLabel.text = classifications.joined(separator: "\n")
+            }
+        }
+        classificationRequest.imageCropAndScaleOption = VNImageCropAndScaleOption.centerCrop
+        
+        self.requests = [classificationRequest]
+    }
+    
+    
+    /// only support back camera
+    var exifOrientationFromDeviceOrientation: Int32 {
+        let exifOrientation: DeviceOrientation
+        enum DeviceOrientation: Int32 {
+            case top0ColLeft = 1
+            case top0ColRight = 2
+            case bottom0ColRight = 3
+            case bottom0ColLeft = 4
+            case left0ColTop = 5
+            case right0ColTop = 6
+            case right0ColBottom = 7
+            case left0ColBottom = 8
+        }
+        switch UIDevice.current.orientation {
+        case .portraitUpsideDown:
+            exifOrientation = .left0ColBottom
+        case .landscapeLeft:
+            exifOrientation = .top0ColLeft
+        case .landscapeRight:
+            exifOrientation = .bottom0ColRight
+        default:
+            exifOrientation = .right0ColTop
+        }
+        return exifOrientation.rawValue
+    }
+    
+    
+    /// resize CVPixelBuffer
+    ///
+    /// - Parameter pixelBuffer: CVPixelBuffer by camera output
+    /// - Returns: CVPixelBuffer with size (299, 299)
+    func resize(pixelBuffer: CVPixelBuffer) -> CVPixelBuffer? {
+        let imageSide = 299
+        var ciImage = CIImage(cvPixelBuffer: pixelBuffer, options: nil)
+        let transform = CGAffineTransform(scaleX: CGFloat(imageSide) / CGFloat(CVPixelBufferGetWidth(pixelBuffer)), y: CGFloat(imageSide) / CGFloat(CVPixelBufferGetHeight(pixelBuffer)))
+        ciImage = ciImage.transformed(by: transform).cropped(to: CGRect(x: 0, y: 0, width: imageSide, height: imageSide))
+        let ciContext = CIContext()
+        var resizeBuffer: CVPixelBuffer?
+        CVPixelBufferCreate(kCFAllocatorDefault, imageSide, imageSide, CVPixelBufferGetPixelFormatType(pixelBuffer), nil, &resizeBuffer)
+        ciContext.render(ciImage, to: resizeBuffer!)
+        return resizeBuffer
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        guard let videoCapture = videoCapture else {return}
+        videoCapture.startCapture()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        guard let videoCapture = videoCapture else {return}
+        videoCapture.resizePreview()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        guard let videoCapture = videoCapture else {return}
+        videoCapture.stopCapture()
+        
+        navigationController?.setNavigationBarHidden(false, animated: true)
+        super.viewWillDisappear(animated)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
